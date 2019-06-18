72daca1cebb2f23a131c6d686a10a00008b4d23f
marda <marda5354@gmail.com>
2019-06-18 05:41:40 +0000

    first commit

---
+++ bbs/app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+	public function Comments()
+	{
+		// 投稿はたくさんのコメントを持つ
+		return $this->hasMany('Comment', 'post_id');
+	}
+
+	public function Category()
+	{
+		// 投稿は1つのカテゴリーに属する
+		return $this->belongsTo('Category','cat_id');
+	}
+}
